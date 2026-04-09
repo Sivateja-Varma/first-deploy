fc55ea4acb4b4fa5af25bf61e12108ac1b797898
Sivateja <jampanasivatejavarma@gmail.com>
2026-04-10 01:28:44 +0530

    last man

--- drinks/settings.py
+++ drinks/settings.py
@@ -125,3 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SAMESITE = 'Lax'
+CSRF_COOKIE_SAMESITE = 'Lax'
